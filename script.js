6a9ba0e25a1103fdce53d69f7d7f58993c6b6102
LaserCooling <lasercooling54@gmail.com>
2026-05-18 08:53:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let port;
 let reader;
+let writer;
 
 let x = [];
 let y1 = [];
@@ -22,7 +23,7 @@ x:x,
 y:y1,
 mode:'lines',
 name:'CH1',
-line:{color:'cyan'}
+line:{color:'#58a6ff',width:2}
 },
 
 {
@@ -30,7 +31,7 @@ x:x,
 y:y2,
 mode:'lines',
 name:'CH2',
-line:{color:'yellow'}
+line:{color:'#3fb950',width:2}
 },
 
 {
@@ -38,24 +39,26 @@ x:x,
 y:y3,
 mode:'lines',
 name:'CH3',
-line:{color:'red'}
+line:{color:'#f85149',width:2}
 }
 
 ],
 
 {
-paper_bgcolor:'#111',
-plot_bgcolor:'#111',
+paper_bgcolor:'#0d1117',
+plot_bgcolor:'#0d1117',
 
-font:{color:'white'},
+font:{color:'#e6edf3'},
+
+xaxis:{
+title:'Samples',
+gridcolor:'#30363d'
+},
 
 yaxis:{
 range:[-2,7],
-title:'Voltage'
-},
-
-xaxis:{
-title:'Samples'
+title:'Voltage',
+gridcolor:'#30363d'
 }
 }
 
@@ -72,9 +75,41 @@ async function connectArduino(){
         baudRate:115200
     });
 
+    writer = port.writable.getWriter();
+
     reader = port.readable.getReader();
 
     readSerial();
+
+    sendFrequencies();
+}
+
+async function sendFrequencies(){
+
+    setInterval(async ()=>{
+
+        let f1 =
+        parseInt(
+        document.getElementById('freq1').value
+        );
+
+        let f2 =
+        parseInt(
+        document.getElementById('freq2').value
+        );
+
+        let f3 =
+        parseInt(
+        document.getElementById('freq3').value
+        );
+
+        let data =
+        new Uint8Array([f1,f2,f3]);
+
+        await writer.write(data);
+
+    },100);
+
 }
 
 async function readSerial(){
@@ -95,13 +130,39 @@ async function readSerial(){
             let ch2 = (value[1]/255)*5;
             let ch3 = (value[2]/255)*5;
 
-            y1.push(ch1 + 2);
+            // CHECKBOXES
+
+            if(
+            document.getElementById('chk1').checked
+            ){
+                y1.push(ch1 + 2);
+            }
+            else{
+                y1.push(null);
+            }
+
             y1.shift();
 
-            y2.push(ch2);
+            if(
+            document.getElementById('chk2').checked
+            ){
+                y2.push(ch2);
+            }
+            else{
+                y2.push(null);
+            }
+
             y2.shift();
 
-            y3.push(ch3 - 2);
+            if(
+            document.getElementById('chk3').checked
+            ){
+                y3.push(ch3 - 2);
+            }
+            else{
+                y3.push(null);
+            }
+
             y3.shift();
 
             Plotly.update('graph',{
